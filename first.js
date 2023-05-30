272c2e0c5ed169d1b5aee919d75f9487af785d86
hastalavista <githumuj@gmail.com>
2023-05-30 12:08:45 +0300

    added object file

--- first.js
+++ first.js
@@ -104,6 +104,16 @@ console.log("After: " + JSON.stringify(testArr));
 //
 */
 
+function abTest(a, b) {
+    if  (a < 0 || b < 0) {
+        return undefined;
+    }
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+console.log(abTest(-2, 2))
+
 
 
 
---
+++ objects.js
@@ -0,0 +1,21 @@
+
+//javascript objects have curly brackets to hold their values
+var testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "drink": "water"
+};
+
+//here we are using bracket notation bacause there's a space in the name
+var entreeValue = testObj["an entree"];
+
+//we can update object properties as follows, from water to tequilla
+testObj.drink = "tequilla";
+
+//here we are using dot notation as there's no space in the name though it can also work in the same case
+var drinkValue = testObj.drink;//drinkValue = tequilla
+
+
+
+
+console.log(drinkValue);//this will print out the new drink value "tequilla"
