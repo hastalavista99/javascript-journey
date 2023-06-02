7169c1a24c09f6d492a28867a064ee5220543aa6
hastalavista <githumuj@gmail.com>
2023-06-02 10:29:27 +0300

    const keyword code

---
+++ const_keyword.js
@@ -0,0 +1,15 @@
+//const is also used to declared variables and has all the features of let
+//variables declared with const are read only and cannot be reassinged after the fact
+
+function funcConst(str) {
+    "use strict";
+
+    const SENTENCE = str + " is cool!";
+    for(let i = 0; i < str.length; i += 2) {
+        console.log(SENTENCE);
+    }
+}
+
+funcConst("Micrologic");
+
+
