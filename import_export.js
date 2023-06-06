550f2281080fac753d601a09df8ccb57a19585b9
hastalavista <githumuj@gmail.com>
2023-06-06 09:27:03 +0300

    default export

--- import_export.js
+++ import_export.js
@@ -1,5 +1,6 @@
 //import is used to get function and variables from another file
 //export is used to give access of functions and variables to another file
+//when importing default export, we dont use the curly braces
 import { capitalizeString } from "./export_file";
 
 const cap = capitalizeString("Hello!");
